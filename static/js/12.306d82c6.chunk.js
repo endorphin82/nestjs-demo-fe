ab540ddcb714277b14fb7782f96b649cbded5b0e
endorphin82 <endorphin82@gmail.com>
2020-12-28 17:58:27 +0200

    Updates

---
+++ static/js/12.306d82c6.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpsaga-template-ts"]=this["webpackJsonpsaga-template-ts"]||[]).push([[12],{217:function(t,e,a){},329:function(t,e,a){"use strict";a.r(e),a.d(e,"default",(function(){return c}));var s=a(0),n=a.n(s);a(217);function c(){return n.a.createElement("div",{className:"some-class"},"Hello React!")}}}]);
+//# sourceMappingURL=12.306d82c6.chunk.js.map
